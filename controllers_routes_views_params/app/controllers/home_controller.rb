5bdc22ea574b8d8af506f2b661ab3abcc6fa823d
malsherer <mgracesherer@gmail.com>
2023-10-03 16:45:57 -0700

    params

--- controllers_routes_views_params/app/controllers/home_controller.rb
+++ controllers_routes_views_params/app/controllers/home_controller.rb
@@ -8,6 +8,16 @@ class HomeController < ApplicationController
     end
     def shaun
     end
+    def cubed
+        @result_number = params[:number].to_i ** 3
+    end
+    def evenly
+        if params[:number1].to_i % params[:number2].to_i == 0
+            @result_string = "Yes"
+        else 
+            @result_string = "no"
+        end
+    end
     def landing
         @tori = 'tori'
         @shaun = 'shaun'
--- controllers_routes_views_params/config/routes.rb
+++ controllers_routes_views_params/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/tori' => 'home#tori'
   get '/shaun' => 'home#shaun'
   get '/mal' => 'home#mal'
+  get '/cubed/:number' => 'home#cubed'
+  get '/evenly/:number1/:number2' => 'home#evenly'
   root 'home#landing'
 end
